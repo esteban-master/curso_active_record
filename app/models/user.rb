eb570cd8a07888fbf92ea6c7367ede1da017d9dc
Esteban Beltran - TCIT <estebanbeltran@tcit.cl>
2022-02-10 11:56:57 -0300

    add has_one with condition,  model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,5 +13,10 @@ class User < ApplicationRecord
   has_many :shopping_carts 
 
   # agregar el modelo en singular
-  has_one :shopping_cart
+  # por defecto has_one de shopping_cart, nos permite obtener el primer elemento del listado de shopping_carts
+  # Al colocar una condicion con una fn lambda, esta se va a aplicar sobre la lista(relacion) shopping_carts
+  # Se lee asi: 
+    # Sobre el listado de carritos de compra que posee el usuario, queremos obtener todos aquellos carritos cuyo atributo 'active' sea igual a 'true'
+  # Usando has_one, solo obtenemos 1 elemento, que seria el 1er carrito de compras que cumpla con la condicion
+  has_one :shopping_cart, -> { where( active: true ).order('id DESC') }
 end
