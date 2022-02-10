03433997e1ca5dabdd9221920a4aa44011f9a099
Esteban Beltran - TCIT <estebanbeltran@tcit.cl>
2022-02-10 09:47:35 -0300

    update model user plus db:migrate

--- db/migrate/20220210124235_create_users.rb
+++ db/migrate/20220210124235_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.string :username
-      t.string :email
+      t.string :username, null: false
+      t.string :email, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220207165604) do
+ActiveRecord::Schema.define(version: 20220210124235) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 20220207165604) do
     t.integer  "price",      default: 0
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username",   null: false
+    t.string   "email",      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
