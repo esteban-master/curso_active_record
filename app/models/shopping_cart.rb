d39e136274f747413f3f9e274270ad2178b85dfe
Esteban Beltran - TCIT <estebanbeltran@tcit.cl>
2022-02-10 10:31:19 -0300

    run comand annotate --models

--- app/models/shopping_cart.rb
+++ app/models/shopping_cart.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: shopping_carts
+#
+#  id         :integer          not null, primary key
+#  total      :integer
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class ShoppingCart < ApplicationRecord
   belongs_to :user
 end
